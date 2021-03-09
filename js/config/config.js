f9727b25c75601ea465800d9cd39cc9f4d31adbe
Humberto Bravo <humbandroid@gmail.com>
2021-03-09 18:40:18 -0300

    Adicionando primeiros recursos da página HTML

--- js/config/config.js
+++ js/config/config.js
@@ -12,8 +12,9 @@ setTimeout(function() {
     window.scrollTo(scrollX, 0);
 }, 300);
 
-// Configurações do menu principal
+// Configurações do menu principal e da barra de menu
 
+// Menu Principal
 const comum_cd = document.getElementById('comum_card');
 const cientifica_cd = document.getElementById('cientifica_card');
 const suprema_cd = document.getElementById('suprema_card');
@@ -29,8 +30,22 @@ grafica_cd.addEventListener("click", function() { realizaAvisoDeIndisponibilidad
 function realizaAvisoDeIndisponibilidade() {
   warning_span.textContent = "Modo indisponível por enquanto. Em Breve novas atualizações!";
   warning_span.style.display = "inline-block";
+
   setTimeout(function() {
     warning_span.style.display = "none";
     warning_span.textContent = "";
-  }, 2000);
+  }, 4000);
 }
+
+// Menu Lateral
+
+const comum_cd_mn = document.getElementById('comum_card_menu');
+const cientifica_cd_mn = document.getElementById('cientifica_card_menu');
+const suprema_cd_mn = document.getElementById('suprema_card_menu');
+const desmontada_cd_mn = document.getElementById('desmontada_card_menu');
+const grafica_cd_mn = document.getElementById('grafica_card_menu');
+
+comum_cd_mn.addEventListener('click', function() { realizaAvisoDeIndisponibilidade(); });
+cientifica_cd_mn.addEventListener('click', function() { realizaAvisoDeIndisponibilidade(); });
+suprema_cd_mn.addEventListener('click', function() { realizaAvisoDeIndisponibilidade(); });
+grafica_cd_mn.addEventListener('click', function() { realizaAvisoDeIndisponibilidade(); });
